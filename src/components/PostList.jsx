dcd5b82c1aeaed11703840f0b00fd8ff48b44951
Сергей Колесников <skx2013@mail.ru>
2025-02-20 21:29:38 +0700

    TransitionGroup

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -17,6 +17,17 @@ const PostList = ({posts, title, remove}) => {
             <h1 style={{ textAlign: 'center' }}>
                 {title}
             </h1>
+            {/*<TransitionGroup>*/}
+            {/*    {posts.map((post, index) =>*/}
+            {/*        <CSSTransition*/}
+            {/*            key={post.id}*/}
+            {/*            timeout={500}*/}
+            {/*            classNames="post"*/}
+            {/*        >*/}
+            {/*            <PostItem remove={remove} number={index + 1} post={post} />*/}
+            {/*        </CSSTransition>*/}
+            {/*    )}*/}
+            {/*</TransitionGroup>*/}
             {posts.map((post, index) =>
                 <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
             )}
