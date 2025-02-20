fe45a4e89ebd5d168159d7f9ff7d05753ecbc64b
Иван Блок <i.blok@2gis.ru>
2025-02-20 22:42:37 +0700

    Правки по версиям пакетов

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,61 @@
-﻿import React, { useState, useMemo } from 'react';
-import Counter from './components/Counter';
-import PostItem from './components/PostItem';
-import PostList from './components/PostList';
-import './styles/App.css';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
-import PostForm from './components/PostForm';
-import MySelect from './components/UI/select/MySelect';
-import PostFilter from './components/PostFilter';
-import MyModal from './components/UI/MyModal/MyModal';
-import { usePosts } from './hooks/usePosts';
-import axios from '../../node_modules/axios/index';
+﻿import React, { useState, useMemo } from "react";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import "./styles/App.css";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
-    const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-    const [filter, setFilter] = useState({ sort: '', query: '' })
+  const [filter, setFilter] = useState({ sort: "", query: "" });
 
-    const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(false);
 
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-        setModal(false)
-    }
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
 
-    async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        console.log(response.data)
-    }
+  async function fetchPosts() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    console.log(response.data);
+  }
 
-    //Получаем пост из дочернего компонента
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
+  //Получаем пост из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
 
-    return (
-        <div className="App">
-            <button onClick={fetchPosts}>GET POSTS</button>
-            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-                Создать пост
-            </MyButton>
-            <MyModal visible={modal} setVisible={setModal}>
-                <PostForm create={createPost} />
-            </MyModal>
-            <hr style={{ margin: '15px 0' }} />
-            <PostFilter
-                filter={filter}
-                setFilter={setFilter}
-            />
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title='JS посты' /> 
-        </div>
-  )
+  return (
+    <div className="App">
+      <button onClick={fetchPosts}>GET POSTS</button>
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="JS посты"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
