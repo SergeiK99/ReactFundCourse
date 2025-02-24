66abd7bfd63b13beca9eca885787b213f054dd4f
Сергей Колесников <skx2013@mail.ru>
2025-02-24 18:08:15 +0700

    Аниманиция и пагинация

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-﻿import React, { useState, useMemo } from "react";
-import Counter from "./components/Counter";
-import PostItem from "./components/PostItem";
+﻿import React, { useState, useMemo, useEffect } from "react";
 import PostList from "./components/PostList";
 import "./styles/App.css";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import { usePosts } from "./hooks/usePosts";
-import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -19,24 +19,37 @@ function App() {
 
   const [modal, setModal] = useState(false);
 
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit));
+  })
+
+  useEffect(() => {
+    fetchPosts(limit, page)
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
 
-  async function fetchPosts() {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-    console.log(response.data);
-  }
-
   //Получаем пост из дочернего компонента
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
+
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts(limit, page)
+  }
 
   return (
     <div className="App">
@@ -49,10 +62,17 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="JS посты"
+      {postError &&
+        <h1>Произошла ошибка ${postError}</h1>
+      }
+      {isPostsLoading
+        ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JS посты" />
+      }
+      <Pagination
+        page={page}
+        changePage={changePage}
+        totalPages={totalPages}
       />
     </div>
   );
